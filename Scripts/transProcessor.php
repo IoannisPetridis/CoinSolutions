f587cf06f6e6e9d150a8515f397efdeafda4c399
Ιωάννης Πετρίδης <mrjohnpetridis@gmail.com>
2018-02-13 00:44:45 +0000

    Commit from home
    Trans processor only remains to do the actuall processing

--- Scripts/transProcessor.php
+++ Scripts/transProcessor.php
@@ -6,24 +6,11 @@ require_once("postFunctions.php");
 checkAuthorised();
 //wget -O /dev/null http://giannispetridis.xyz/CoinSolutions/Scripts/transProcessor.php?pass=masterpass
 if (mysqli_select_db($con,$dbname)) {
-	$sql = "CREATE EVENT testevent ON SCHEDULE AT CURRENT_TIMESTAMP EVERY 1 SECOND DO (SELECT ) ";
+	$sql = "SELECT * FROM Transaction WHERE State=0 ORDER BY Transaction.Timestamp_created ASC";
 	if ($result=mysqli_query($con,$sql)) {
-		// Return the number of rows in result set
-		$rowcount = mysqli_num_rows($result);
-		if ($rowcount ==0) {
-			die("Transaction doesn't exist!");
+		while ($row = mysqli_fetch_array($result)) {
+			//TODO: The actual processing
 		}
-		else {
-			$ar = array();
-			while ($row = mysqli_fetch_array($result)) {
-				$obj = new stdClass();
-				$obj->State = $row['State'];
-				
-				array_push($ar,$obj);
-			}
-			echo json_encode($ar);
-		}
-		mysqli_free_result($result);
 	}
 	else {
 		echo mysqli_error($con);
